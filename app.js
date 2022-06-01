21fd38ccdca05538d35dcb3a87d62ebef948669c
toremann <50668920+toremann@users.noreply.github.com>
2022-06-01 20:18:50 +0200

    custom headers

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-require('dotenv').config()
-const express = require('express');
-const morgan = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
-var ip = require('ip');
-app.use(morgan('short'));
+var ip = require("ip");
+app.use(morgan("short"));
 
-const port = '3001'
-const apiKey = process.env.API_KEY
+const port = "3001";
+const apiKey = process.env.API_KEY;
 
 // With git bash:
 // curl http://localhost:3001/secret/ -H 'x-api-key: fooBar123$'
 
 app.get("/secret/", (req, res) => {
-    console.log('this the api-key:', apiKey)
-    console.log(req.headers)
-      res.status(500).send(apiKey);
-    }
-  );
-
+  const reqKey = req.headers["x-api-key"];
+  if (reqKey == apiKey) {
+    res.status(500).send("Authorized");
+  } else {
+    res.status(401).send("Access denied!");
+  }
+});
 
 app.listen(port, () => {
-    console.log('Server started', ip.address(),port);
-  });
+  console.log("Server started", ip.address(), port);
+});
